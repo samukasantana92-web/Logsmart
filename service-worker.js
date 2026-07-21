6fccef2cb1d481376acd3822a748473e58f2896f
samukasantana92-web <samukasantana92@gmail.com>
2026-07-20 22:37:48 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "logsmart-v1";
+const CACHE_NAME = "logsmart-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -10,6 +10,7 @@ const FILES_TO_CACHE = [
   "./erro.html",
   "./css/style.css",
   "./js/script.js",
+  "./manifest.json",
   "./img/logo.png",
   "./img/icon-192.png",
   "./img/icon-512.png"
@@ -19,6 +20,19 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
